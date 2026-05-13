36da8fd2b25a92b015498f25f53e2f9eac39dc92
lweber89 <lweber89@gmail.com>
2026-05-13 15:43:09 -0500

    Worked on Lab 6:  add_vector

--- geog510_lab_utils/geog510_lab_utils.py
+++ geog510_lab_utils/geog510_lab_utils.py
@@ -13,3 +13,47 @@ class Map(ipyleaflet.Map):
         url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
         layer = ipyleaflet.TileLayer(url=url, name=basemap)
         self.add_layer(layer)
+
+    def add_layer_control(self):
+        print("You are attempting to add_layer_control")
+
+    def add_geojson(self, data, hover_style=None, **kwargs):
+
+        import geopandas as gpd
+
+        if hover_style is None:
+            hover_style = {"color": "green", "fillOpacity": 0.2}
+
+        if isinstance(data, str):
+            gdf = gpd.read_file(data)
+            geojson = gdf.__geo_interface__
+
+        elif isinstance(data, dict):
+            geojson = data
+
+        layer = ipyleaflet.GeoJSON(data=geojson, hover_style=hover_style, **kwargs)
+        self.add_layer(layer)
+
+    def add_gdf(self, gdf, **kwargs):
+
+        gdf = gdf.to_crs(epsg=4326)
+        geojson = gdf.__geo_interface__
+
+        self.add_geojson(geojson, **kwargs)
+
+    def add_vector(self, data, **kwargs):
+        import geopandas as gpd
+
+        if isinstance(data, str):
+            gdf = gpd.read_file(data)
+
+        elif isinstance(data, gpd.GeoDataFrame):
+            gdf = data
+
+        elif isinstance(data, dict):
+            return self.add_geojson(data, **kwargs)
+
+        else:
+            raise ValueError("Invalid data type.")
+
+        self.add_gdf(gdf, **kwargs)
